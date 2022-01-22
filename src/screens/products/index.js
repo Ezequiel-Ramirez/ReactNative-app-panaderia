74d1892584b976740ec660ebc4ba36e1213ad944
Ezequiel Emanuel Ramirez <ezequielram@gmail.com>
2022-01-22 11:03:10 -0300

    implemento botones para navegar en cada stack

--- src/screens/products/index.js
+++ src/screens/products/index.js
@@ -1,13 +1,14 @@
-import { SafeAreaView, Text, View } from 'react-native';
+import { Button, SafeAreaView, Text, View } from 'react-native';
 
 import React from 'react';
 import styles from './style';
 
-const index = () => {
+const index = ({navigation}) => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
         <Text>Product</Text>
+        <Button title="Go to Details" onPress={() => navigation.navigate('ProductDetail')} />
       </View>
     </SafeAreaView>
   );
