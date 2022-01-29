c3d74786994ea85d1962bfabae3cb38b4be075c7
Ezequiel Emanuel Ramirez <ezequielram@gmail.com>
2022-01-28 21:55:44 -0300

    iconos en tab nav

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,4 +1,5 @@
 import CartStackNavigation from './cart';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import ShopStackNavigation from './shop';
@@ -10,10 +11,33 @@ const BottomTabs = createBottomTabNavigator();
 const AppNavigation = () => {
     return (
         <NavigationContainer>
-           <BottomTabs.Navigator initialRouteName='Shop'>
-                <BottomTabs.Screen name='Shop' component={ShopStackNavigation} />
-                <BottomTabs.Screen name='Cart' component={CartStackNavigation} />
-           </BottomTabs.Navigator>
+            <BottomTabs.Navigator initialRouteName='Shop' screenOptions={{
+                headerShown: false,
+            }} >
+                <BottomTabs.Screen
+                    name='Shop'
+                    component={ShopStackNavigation}
+                    options={
+                        {
+                            tabBarIcon: ({ focused }) => (
+                                <Ionicons name='home' size={26} color={focused ? '#f4511e' : 'gray'} />
+                            ),
+
+                        }
+                    } />
+
+                <BottomTabs.Screen
+                    name='Cart'
+                    component={CartStackNavigation}
+                    options={
+                        {
+                            tabBarIcon: ({ focused }) => (
+                                <Ionicons name='cart' size={26} color={focused ? '#f4511e' : 'gray'} />
+                            ),
+
+                        }
+                    } />
+            </BottomTabs.Navigator>
         </NavigationContainer>
     )
 }
